556d448858b68b6eb735dca901f173eef7f9a0f2
Erdem Ersoy <erdemersoy256@gmail.com>
2023-12-17 08:46:12 +0300

    Improve project template

--- version.py
+++ version.py
@@ -1,13 +1,12 @@
 class Version:
-    PROJECT_NAME = "Ersoy Kardesler Win32 Subsystem"
-    VERSION = "0.0.0"
-
     def get_project_name():
         with open("PROJECT_NAME", "r") as f:
-            return f.readline().strip()
+            PROJECT_NAME = f.readline().strip()
+            return PROJECT_NAME
 
     def get_version():
         with open("VERSION", "r") as f:
-            return f.readline().strip()
+            PROJECT_VERSION = f.readline().strip()
+            return PROJECT_VERSION
 
 
